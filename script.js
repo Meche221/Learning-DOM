5f2f0326282fd592cc479817a91fff297b03faa5
Meche221 <binaymeche22@gmail.com>
2025-07-05 22:41:34 +0545

    All done

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ function subscribe(){
       buttonElement.innerHTML = 'Subscribe'
     }
 }
+
+function calculateTotal(){
+ const inputElement = document.querySelector('#js-cost-input');
+ let cost = Number(inputElement.value);
+
+ if (cost < 40) {
+  cost = cost + 10;
+ }
+
+ document.querySelector('#js-total-cost')
+ .innerHTML = `$${cost}`; 
+}
+
+document.querySelector('#js-cost-input').addEventListener('keydown', function(event) {
+  if (event.key === 'Enter') {
+    calculateTotal();
+  }
+});
